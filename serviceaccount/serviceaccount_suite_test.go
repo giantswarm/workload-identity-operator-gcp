a963157c8cdf8edf53b051e131188039ba03f52f
George Gelaga-King <gb7309@gmail.com>
2022-07-18 07:16:53 +0100

    Add: Service account reconciler tests

---
+++ serviceaccount/serviceaccount_suite_test.go
@@ -0,0 +1,75 @@
+package serviceaccount_test
+
+import (
+	"context"
+	"testing"
+
+	"github.com/google/uuid"
+	. "github.com/onsi/ginkgo/v2"
+	. "github.com/onsi/gomega"
+	corev1 "k8s.io/api/core/v1"
+	"k8s.io/client-go/kubernetes/scheme"
+	"sigs.k8s.io/controller-runtime/pkg/client"
+	"sigs.k8s.io/controller-runtime/pkg/envtest"
+	logf "sigs.k8s.io/controller-runtime/pkg/log"
+	"sigs.k8s.io/controller-runtime/pkg/log/zap"
+
+	"github.com/giantswarm/workload-identity-operator-gcp/tests"
+	//+kubebuilder:scaffold:imports
+)
+
+func TestK8s(t *testing.T) {
+	RegisterFailHandler(Fail)
+	RunSpecs(t, "K8s Suite")
+}
+
+// These tests use Ginkgo (BDD-style Go testing framework). Refer to
+// http://onsi.github.io/ginkgo/ to learn more about Ginkgo.
+var (
+	k8sClient client.Client
+	testEnv   *envtest.Environment
+	namespace string
+)
+
+var _ = BeforeSuite(func() {
+	logf.SetLogger(zap.New(zap.WriteTo(GinkgoWriter), zap.UseDevMode(true)))
+
+	tests.GetEnvOrSkip("KUBEBUILDER_ASSETS")
+
+	By("bootstrapping test environment")
+	testEnv = &envtest.Environment{
+		ErrorIfCRDPathMissing: true,
+	}
+
+	cfg, err := testEnv.Start()
+	Expect(err).NotTo(HaveOccurred())
+	Expect(cfg).NotTo(BeNil())
+
+	//+kubebuilder:scaffold:scheme
+
+	k8sClient, err = client.New(cfg, client.Options{Scheme: scheme.Scheme})
+	Expect(err).NotTo(HaveOccurred())
+	Expect(k8sClient).NotTo(BeNil())
+})
+
+var _ = AfterSuite(func() {
+	By("tearing down the test environment")
+	if testEnv == nil {
+		return
+	}
+	err := testEnv.Stop()
+	Expect(err).NotTo(HaveOccurred())
+})
+
+var _ = BeforeEach(func() {
+	namespace = uuid.New().String()
+	namespaceObj := &corev1.Namespace{}
+	namespaceObj.Name = namespace
+	Expect(k8sClient.Create(context.Background(), namespaceObj)).To(Succeed())
+})
+
+var _ = AfterEach(func() {
+	namespaceObj := &corev1.Namespace{}
+	namespaceObj.Name = namespace
+	Expect(k8sClient.Delete(context.Background(), namespaceObj)).To(Succeed())
+})
---
+++ serviceaccount/serviceaccount_test.go
@@ -0,0 +1,169 @@
+package serviceaccount_test
+
+import (
+	"context"
+	"fmt"
+
+	. "github.com/onsi/ginkgo/v2"
+	. "github.com/onsi/gomega"
+	corev1 "k8s.io/api/core/v1"
+	k8serrors "k8s.io/apimachinery/pkg/api/errors"
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+	"sigs.k8s.io/controller-runtime/pkg/client"
+
+	"github.com/giantswarm/workload-identity-operator-gcp/serviceaccount"
+	"github.com/giantswarm/workload-identity-operator-gcp/webhook"
+)
+
+var _ = Describe("Service Account Reconcilation", func() {
+	var (
+		ctx context.Context
+
+		serviceAccount     *corev1.ServiceAccount
+		serviceAccountName = "the-service-account"
+
+		gcpServiceAccount    = "service-account@email"
+		workloadIdentityPool = "workload-identity-pool-id"
+		identityProvider     = "https://gkehub.googleapis.com/projects/testing-1234/locations/global/memberships/cluster"
+
+		secret     *corev1.Secret
+		secretName = fmt.Sprintf("%s-%s", serviceAccountName, serviceaccount.SecretNameSuffix)
+	)
+
+	When("a correctly annotated service account is created", func() {
+		BeforeEach(func() {
+			ctx = context.Background()
+
+			serviceAccount = &corev1.ServiceAccount{
+				ObjectMeta: metav1.ObjectMeta{
+					Name:      serviceAccountName,
+					Namespace: namespace,
+					Annotations: map[string]string{
+						webhook.AnnotationGCPServiceAccount:      gcpServiceAccount,
+						webhook.AnnotationWorkloadIdentityPoolID: workloadIdentityPool,
+						webhook.AnnotationGCPIdentityProvider:    identityProvider,
+					},
+				},
+			}
+			Expect(k8sClient.Create(ctx, serviceAccount)).To(Succeed())
+		})
+
+		JustBeforeEach(func() {
+			secret = nil
+			err := k8sClient.Get(ctx, client.ObjectKey{
+				Namespace: namespace,
+				Name:      secretName,
+			}, secret)
+
+			Expect(err).NotTo(HaveOccurred())
+		})
+
+		It("should create a secret with the correct credentials", func() {
+			Expect(secret).ToNot(BeNil())
+			Expect(secret.Name).To(Equal(secretName))
+			Expect(secret.Namespace).To(Equal(namespace))
+
+			data := secret.StringData["config"]
+
+			expectedData := fmt.Sprintf(`{
+	     "type": "external_account",
+	     "audience": "identitynamespace:%[1]s:%[2]s",
+	     "service_account_impersonation_url": "https://iamcredentials.googleapis.com/v1/projects/-/serviceAccounts/%[3]s:generateAccessToken",
+	     "subject_token_type": "urn:ietf:params:oauth:token-type:jwt",
+	     "token_url": "https://sts.googleapis.com/v1/token",
+	     "credential_source": {
+	       "file": "/var/run/secrets/tokens/gcp-ksa/token"
+	     }
+	   }`, workloadIdentityPool, identityProvider, gcpServiceAccount)
+
+			Expect(data).Should(MatchJSON(expectedData))
+		})
+	})
+
+	When("a service account without a workload identity pool is created", func() {
+		BeforeEach(func() {
+			ctx = context.Background()
+
+			serviceAccount = &corev1.ServiceAccount{
+				ObjectMeta: metav1.ObjectMeta{
+					Name:      serviceAccountName,
+					Namespace: namespace,
+					Annotations: map[string]string{
+						webhook.AnnotationGCPServiceAccount:   gcpServiceAccount,
+						webhook.AnnotationGCPIdentityProvider: identityProvider,
+					},
+				},
+			}
+			Expect(k8sClient.Create(ctx, serviceAccount)).To(Succeed())
+		})
+
+		It("should not create a secret", func() {
+			secret = nil
+			err := k8sClient.Get(ctx, client.ObjectKey{
+				Namespace: namespace,
+				Name:      secretName,
+			}, secret)
+
+			Expect(err).To(HaveOccurred())
+			Expect(k8serrors.IsNotFound(err)).To(BeTrue())
+		})
+	})
+
+	When("a service account without a gcpServiceAccount annotation is created", func() {
+		BeforeEach(func() {
+			ctx = context.Background()
+
+			serviceAccount = &corev1.ServiceAccount{
+				ObjectMeta: metav1.ObjectMeta{
+					Name:      serviceAccountName,
+					Namespace: namespace,
+					Annotations: map[string]string{
+						webhook.AnnotationWorkloadIdentityPoolID: workloadIdentityPool,
+						webhook.AnnotationGCPIdentityProvider:    identityProvider,
+					},
+				},
+			}
+			Expect(k8sClient.Create(ctx, serviceAccount)).To(Succeed())
+		})
+
+		It("should not create a secret", func() {
+			secret = nil
+			err := k8sClient.Get(ctx, client.ObjectKey{
+				Namespace: namespace,
+				Name:      secretName,
+			}, secret)
+
+			Expect(err).To(HaveOccurred())
+			Expect(k8serrors.IsNotFound(err)).To(BeTrue())
+		})
+	})
+
+	When("a service account without an identity provider annotation is created", func() {
+		BeforeEach(func() {
+			ctx = context.Background()
+
+			serviceAccount = &corev1.ServiceAccount{
+				ObjectMeta: metav1.ObjectMeta{
+					Name:      serviceAccountName,
+					Namespace: namespace,
+					Annotations: map[string]string{
+						webhook.AnnotationGCPServiceAccount:      gcpServiceAccount,
+						webhook.AnnotationWorkloadIdentityPoolID: workloadIdentityPool,
+					},
+				},
+			}
+			Expect(k8sClient.Create(ctx, serviceAccount)).To(Succeed())
+		})
+
+		It("should not create a secret", func() {
+			secret = nil
+			err := k8sClient.Get(ctx, client.ObjectKey{
+				Namespace: namespace,
+				Name:      secretName,
+			}, secret)
+
+			Expect(err).To(HaveOccurred())
+			Expect(k8serrors.IsNotFound(err)).To(BeTrue())
+		})
+	})
+})
