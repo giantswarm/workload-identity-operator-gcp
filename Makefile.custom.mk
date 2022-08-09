36376fa298b7f0d46c9ba3d5f917702e61e8029f
George Gelaga-King <gb7309@gmail.com>
2022-08-09 20:28:29 +0100

    Add: cluster reconciler

--- Makefile.custom.mk
+++ Makefile.custom.mk
@@ -87,7 +87,7 @@ build: generate fmt vet ## Build manager binary.
 	go build -o bin/manager main.go
 
 .PHONY: run
-run: manifests generate fmt vet ## Run a controller from your host.
+run: fmt vet ## Run a controller from your host.
 	go run ./main.go
 
 .PHONY: docker-build
--- controllers/serviceaccount.go
+++ controllers/serviceaccount.go
@@ -1,4 +1,4 @@
-package serviceaccount
+package controllers
 
 import (
 	"context"
