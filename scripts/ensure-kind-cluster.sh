63dfeb9eeebc7a8abba7d7493e3b4dde9efd17db
George Gelaga-King <gb7309@gmail.com>
2022-08-30 10:20:10 +0100

    Update: tests

--- scripts/ensure-kind-cluster.sh
+++ scripts/ensure-kind-cluster.sh
@@ -4,15 +4,19 @@ set -euo pipefail
 
 readonly SCRIPT_DIR="$(cd "$(dirname "$0")" && pwd)"
 readonly REPO_ROOT="${SCRIPT_DIR}/.."
-readonly CLUSTER=${CLUSTER:-"dns-operator-gcp-acceptance"}
+readonly CLUSTER=${CLUSTER:-"acceptance"}
 readonly KIND="${REPO_ROOT}/bin/kind"
-readonly IMG=${IMG:-quay.io/giantswarm/dns-operator-gcp:latest}
+readonly CLUSTERCTL="${REPO_ROOT}/bin/clusterctl"
+readonly IMG=${IMG:-quay.io/giantswarm/workload-identity-operator-gcp:latest}
+readonly WORKLOAD_CLUSTER="acceptance-workload-cluster"
 
 ensure_kind_cluster() {
   local cluster
   cluster="$1"
   if ! "$KIND" get clusters | grep -q "$cluster"; then
-    "$KIND" create cluster --name "$cluster" --wait 5m
+    export CLUSTER_TOPOLOGY=true
+
+    "$KIND" create cluster --name "$cluster" --config "${SCRIPT_DIR}/assets/kind-cluster-with-extramounts.yaml" --wait 5m
   fi
   "$KIND" export kubeconfig --name "$cluster" --kubeconfig "$HOME/.kube/$cluster.yml"
 }
@@ -32,4 +36,22 @@ helm upgrade --install \
   --wait
 
 kubectl apply -f "${SCRIPT_DIR}/assets/cluster-issuer.yaml"
+
+# "$CLUSTERCTL" init --infrastructure docker
+clusterctl init --infrastructure docker
+
+echo "---> Waiting for CAPI controller deployments to be ready"
+
+set -x
+
+kubectl -n "capd-system" rollout status "deploy/capd-controller-manager"
+kubectl -n capi-kubeadm-bootstrap-system rollout status deploy/capi-kubeadm-bootstrap-controller-manager
+kubectl -n capi-kubeadm-control-plane-system rollout status deploy/capi-kubeadm-control-plane-controller-manager
+kubectl -n capi-system rollout status deploy/capi-controller-manager
+
+{ set +x; } 2> /dev/null
+
+kubectl apply -f "${SCRIPT_DIR}/assets/workload-cluster.yaml"
+
+"$KIND" load docker-image --name "$WORKLOAD_CLUSTER" "$IMG"
 
