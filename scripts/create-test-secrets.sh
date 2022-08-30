77b76b73e290f5d5a9acb8f60e173fcfd0cddc4e
George Gelaga-King <gb7309@gmail.com>
2022-08-30 10:20:10 +0100

    Update: tests

--- scripts/create-test-secrets.sh
+++ scripts/create-test-secrets.sh
@@ -2,8 +2,19 @@
 
 set -euo pipefail
 
+TMPDIR=$(mktemp -d)
+
 readonly SCRIPT_DIR="$(cd "$(dirname "$0")" && pwd)"
-readonly SECRET_NAME="$CLUSTER-kubeconfig"
+readonly REPO_ROOT="${SCRIPT_DIR}/.."
+readonly CLUSTERCTL="${REPO_ROOT}/bin/clusterctl"
+readonly WORKLOAD_CLUSTER="acceptance-workload-cluster"
+readonly SECRET_NAME="$WORKLOAD_CLUSTER-kubeconfig"
+
+clusterctl get kubeconfig $WORKLOAD_CLUSTER > "$HOME/.kube/workload-cluster.yaml"
+
+
+kubectl --kubeconfig="$HOME/.kube/workload-cluster.yaml" apply -f https://docs.projectcalico.org/v3.21/manifests/calico.yaml
+kubectl --kubeconfig="$HOME/.kube/workload-cluster.yaml" create namespace giantswarm || true
 
 cat <<EOF | kubectl apply -f -
 apiVersion: v1
@@ -13,7 +24,7 @@ metadata:
   namespace: "giantswarm"
 type: Opaque
 data:
-  value: $( cat ${HOME}/.kube/$CLUSTER.yml | base64 | tr -d '\n' )
+  value: $( cat "$HOME/.kube/workload-cluster.yaml" | base64 | tr -d '\n' )
 EOF
 
 cat <<EOF | kubectl apply -f -
@@ -24,6 +35,34 @@ metadata:
   namespace: "giantswarm"
 type: Opaque
 data:
-  file.json: $( cat ${SCRIPT_DIR}/assets/test_creds.json | base64 | tr -d '\n' )
+  file.json: $( cat ${SCRIPT_DIR}/assets/test-credentials.json | base64 | tr -d '\n' )
+EOF
+
+
+# Point the kubeconfig to the exposed port of the load balancer, rather than the inaccessible container IP.
+sed -i -e "s/server:.*/server: https:\/\/$(docker port ${WORKLOAD_CLUSTER}-lb 6443/tcp | sed "s/0.0.0.0/127.0.0.1/")/g" "$HOME/.kube/workload-cluster.yaml"
+
+helm repo add jetstack https://charts.jetstack.io
+helm repo update
+helm upgrade --install \
+  cert-manager jetstack/cert-manager \
+	--kubeconfig="$HOME/.kube/workload-cluster.yaml" \
+  --namespace cert-manager \
+  --create-namespace \
+  --version v1.8.0 \
+  --set installCRDs=true \
+  --wait
+
+kubectl --kubeconfig="$HOME/.kube/workload-cluster.yaml" apply -f "${SCRIPT_DIR}/assets/cluster-issuer.yaml"
+
+cat <<EOF | kubectl --kubeconfig="$HOME/.kube/workload-cluster.yaml" apply -f -
+apiVersion: v1
+kind: Secret
+metadata:
+  name: "gcp-credentials"
+  namespace: "giantswarm"
+type: Opaque
+data:
+  file.json: $( cat ${SCRIPT_DIR}/assets/test-credentials.json | base64 | tr -d '\n' )
 EOF
 
