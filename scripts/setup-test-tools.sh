8c2f417adf3206a98693d10693bb171ac7671e49
George Gelaga-King <gb7309@gmail.com>
2022-08-30 15:27:26 +0100

    Update: Circle CI Config

--- scripts/setup-test-tools.sh
+++ scripts/setup-test-tools.sh
@@ -2,6 +2,6 @@
 
 set -euo pipefail
 
-curl -L https://github.com/kubernetes-sigs/cluster-api/releases/download/v0.3.6/clusterctl-linux-amd64 -o clusterctl
+curl -L https://github.com/kubernetes-sigs/cluster-api/releases/download/v1.2.1/clusterctl-linux-amd64 -o clusterctl
 chmod +x ./clusterctl
 sudo mv ./clusterctl /usr/local/bin/clusterctl
